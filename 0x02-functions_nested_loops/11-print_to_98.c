12a7f81fffa3623a71ac42c7137686d0fb98ba2c
MelanieSaina <Melaniecherono@gmail.com>
2023-02-21 10:09:48 -0800

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,7 +1,13 @@
 #include "main.h"
 #include "6-abs.c"
 #include <stdio.h>
-
+/**
+ *@i integer to be compared with
+ *
+ *print_to_98(int n)- to print all numbers from n to 98
+ *
+ * Return 0
+ * */
 void print_to_98(int n)
 {
 	int i;
