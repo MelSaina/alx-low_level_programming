11ebdfa031dc178d42f783833eee1d3daad6552a
MelanieSaina <Melaniecherono@gmail.com>
2023-03-10 12:00:37 -0800

    Task completed

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.c>
+#include <ctype.h>
 
 /**
  * main - Program that takes in all integer arguments and returns the sum
