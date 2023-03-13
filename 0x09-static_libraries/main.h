d91b052a43d2c7e2c2e6ce75791e84d93851201d
MelanieSaina <Melaniecherono@gmail.com>
2023-03-13 06:33:22 -0700

    corrections

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,3 +1,5 @@
+#ifndef main_h
+#define main_h
 /*
  * File: holberton.h
  * Auth: Firdaus
@@ -25,3 +27,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+
+#endif
