446cfe21981c3048c1dfc47cf4cda7b9c9696cbe
MelSaina <81955638+MelSaina@users.noreply.github.com>
2023-03-31 21:01:39 +0300

    Corrections

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,20 +1,18 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
 /**
- * list_len - length of list_t struct
- * @h: head node
- * Return: number of elements
+ * list_len-length of list_t struct
+ * @h:head node
+ * Return:number of elements
  */
 size_t list_len(const list_t *h)
 {
 	int count = 0;
-
-	while (h != NULL)
+	while (h !=NULL)
 	{
 		count++;
-		h = h->next;
+		h=h->next;
 	}
-
 	return (count);
 }
