1e647b7742164c04974eb68a31338927e0ffd52a
MelSaina <81955638+MelSaina@users.noreply.github.com>
2023-03-31 21:15:12 +0300

    corrections

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,42 +1,43 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
 #include "lists.h"
-/**
- * add_node_end - adds node at end
- * @head: head
- * @str: string to add
- * Return: address
- */
-list_t *add_node_end(list_t **head, const char *str)
-{
-	int i = 0;
-	list_t *new, *f;
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-	while (str[i])
+/**
+ * add_node_end-adds node at end
+ * @head:head
+ * @str:string to add
+ * Return:address
+ */
+list_t *add_node_end(list_t **head,const char *str)
+{
+	int i=0;
+	list_t *new,*f;
+
+	while(str[i])
 		i++;
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
-		return (NULL);
-	new->str = strdup(str);
-	new->len = i;
-	new->next = NULL;
-	if (strdup(str) == NULL)
+	new=malloc(sizeof(list_t));
+	if(new==NULL)
+		return(NULL);
+	new->str=strdup(str);
+	new->len=i;
+	new->next=NULL;
+	if (strdup(str)==NULL)
 	{
 		free(new);
-		return (NULL);
+		return(NULL);
 	}
-	if (*head == NULL)
+	if (*head==NULL)
 	{
-		*head = new;
-		return (new);
+		*head=new;
+		return(new);
 	}
 	else
 	{
-		f = *head;
-		while (f->next != NULL)
-			f = f->next;
-		f->next = new;
-		return (new);
+		f=*head;
+		while(f->next !=NULL)
+			f=f->next;
+		f->next=new;
+		return(new);
 	}
 }
