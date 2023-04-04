f52b7ebb2d77fffebb027cd9d5632faa95b10bca
Melanie Saina <Gabbysiana@gmail.com>
2023-04-04 00:26:44 -0700

    Corrrections

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,11 +10,12 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t nd = 0;
+
 	while (h)
 		{
 			nd++;
-			printf( "%d\n", h -> n  );
-			h =  h -> next;
-		}	
+			printf("%d\n", h->n);
+			h = h->next;
+		}
 	return (nd);
 }
