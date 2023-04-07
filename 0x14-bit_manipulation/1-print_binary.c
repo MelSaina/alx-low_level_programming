5ff745c1dcaa256676d1351fd0d8ee68ec8cc721
Melanie Saina <Gabbysiana@gmail.com>
2023-04-07 11:54:43 -0700

    1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,3 +6,22 @@
  * Return:  Binary- 0 or 1.
  */
 
+void print_binary(unsigned long int n)
+{
+	unsigned long int m   = 1;
+	int i;
+
+	for (i = 0; (n >> i) != 0; i++)
+		;
+	for (; i >= 0; i--)
+	{
+		if ((n & (m << i)) != 0)
+		{
+			_putchar('1');
+		}
+		else
+		{
+			_putchar('0');
+		}
+	}
+}
